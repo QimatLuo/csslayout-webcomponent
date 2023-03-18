51fea86c979f1e98d628294c00e8293c52e739f7
Qimat Luo <qimatluo@westpharma.com>
2023-03-19 04:35:46 +0800

    Support toggle expand

--- vite/src/WebComponent/accordion-item/index.ts
+++ vite/src/WebComponent/accordion-item/index.ts
@@ -12,4 +12,37 @@ export class AccordionItem extends HTMLElement {
       template.content.cloneNode(true)
     );
   }
+
+  connectedCallback() {
+    this.shadowRoot
+      ?.querySelector<HTMLElement>(".header")
+      ?.addEventListener("click", toggleExpand(this));
+  }
+  disconnectedCallback() {
+    this.shadowRoot
+      ?.querySelector<HTMLElement>(".header")
+      ?.removeEventListener("click", toggleExpand(this));
+  }
+
+  get expanded() {
+    return (
+      this.shadowRoot
+        ?.querySelector(".item")
+        ?.classList.contains("item--expanded") || false
+    );
+  }
+  set expanded(b: boolean) {
+    const classList = this.shadowRoot?.querySelector(".item")?.classList;
+    if (b) {
+      classList?.replace("item--collapsed", "item--expanded");
+    } else {
+      classList?.replace("item--expanded", "item--collapsed");
+    }
+  }
+}
+
+function toggleExpand(host: AccordionItem) {
+  return (e: MouseEvent) => {
+    host.expanded = !host.expanded;
+  };
 }
