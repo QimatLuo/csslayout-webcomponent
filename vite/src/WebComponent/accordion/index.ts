ede9d62528f003c020d522bcc9636dc8bd24bd5e
Qimat Luo <qimatluo@westpharma.com>
2023-03-19 03:49:48 +0800

    Add <accordion>

---
+++ vite/src/WebComponent/accordion/index.ts
@@ -0,0 +1,15 @@
+import STYLE from "./style.css?raw";
+import TEMPLATE from "./template.html?raw";
+
+export class Accordion extends HTMLElement {
+  constructor() {
+    super();
+
+    const template = document.createElement("template");
+    template.innerHTML = `<style>${STYLE}</style>${TEMPLATE}`;
+
+    this.attachShadow({ mode: "open" }).appendChild(
+      template.content.cloneNode(true)
+    );
+  }
+}
--- vite/src/main.ts
+++ vite/src/main.ts
@@ -1,4 +1,7 @@
 import "./style.css";
 import TEMPLATE from "./template.html?raw";
 
+import { Accordion } from "./WebComponent/accordion";
+customElements.define("my-accordion", Accordion);
+
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = TEMPLATE;
