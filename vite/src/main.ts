b4786baadf33c6dc13eecc4c32384f82dfc5c11d
Qimat Luo <qimatluo@westpharma.com>
2023-03-19 07:09:54 +0800

    Use register helper

--- vite/src/main.ts
+++ vite/src/main.ts
@@ -1,10 +1,13 @@
 import "./style.css";
 import TEMPLATE from "./template.html?raw";
+import { register } from "./WebComponent/util";
 
-import { Accordion } from "./WebComponent/accordion";
-customElements.define("my-accordion", Accordion);
+Promise.all(
+  Array.of(
+    "./WebComponent/accordion",
+    "./WebComponent/accordion-item",
+  ).map((x) => import(x).then(register("my")))
+).then(() => {
+  document.querySelector<HTMLDivElement>("#app")!.innerHTML = TEMPLATE;
+});
 
-import { AccordionItem } from "./WebComponent/accordion-item";
-customElements.define("my-accordion-item", AccordionItem);
-
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = TEMPLATE;
