f13f373ff08482f710f79452294e50514d0f0326
Qimat Luo <qimatluo@westpharma.com>
2023-03-19 11:48:10 +0800

    Add <spinner>

---
+++ vite/src/WebComponent/spinner/index.ts
@@ -0,0 +1,16 @@
+import STYLE from "./style.css?raw";
+import TEMPLATE from "./template.html?raw";
+
+export const tagName = "spinner";
+export class WebComponent extends HTMLElement {
+  constructor() {
+    super();
+
+    const template = document.createElement("template");
+    template.innerHTML = `<style>${STYLE}</style>${TEMPLATE}`;
+
+    this.attachShadow({ mode: "open" }).appendChild(
+      template.content.cloneNode(true)
+    );
+  }
+}
