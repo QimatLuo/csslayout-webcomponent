1590671d5591ef6a51d64f826ccbd2fbe4231634
Qimat Luo <qimatluo@westpharma.com>
2023-03-19 08:16:02 +0800

    Add example

--- vite/src/main.ts
+++ vite/src/main.ts
@@ -1,13 +1,24 @@
 import "./style.css";
 import TEMPLATE from "./template.html?raw";
 import { register } from "./WebComponent/util";
+import { WebComponent as Accordion } from "./WebComponent/accordion";
 
 Promise.all(
   Array.of(
     "./WebComponent/accordion",
     "./WebComponent/accordion-item",
+    "./WebComponent/spinner",
+    "./components/form-login"
   ).map((x) => import(x).then(register("my")))
 ).then(() => {
-  document.querySelector<HTMLDivElement>("#app")!.innerHTML = TEMPLATE;
-});
+  const app = document.querySelector<HTMLDivElement>("#app");
+  app!.innerHTML = TEMPLATE;
 
+  const accordion = app?.querySelector<Accordion>("my-accordion");
+  accordion!.step = 1;
+
+  const formLogin = app?.querySelector<Accordion>("my-form-login");
+  formLogin?.addEventListener("login", () => {
+    accordion!.step = 2;
+  });
+});
